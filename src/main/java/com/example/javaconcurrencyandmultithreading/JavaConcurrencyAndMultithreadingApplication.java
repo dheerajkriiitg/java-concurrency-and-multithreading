11a2eaf2c7c9aeb1a0425b882e50f99047938b4f
Dheeraj Kumar <dheeraj.kumar1@swiggy.in>
2024-02-07 09:32:10 +0530

    initial commit

---
+++ src/main/java/com/example/javaconcurrencyandmultithreading/JavaConcurrencyAndMultithreadingApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaconcurrencyandmultithreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConcurrencyAndMultithreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaConcurrencyAndMultithreadingApplication.class, args);
+	}
+
+}
